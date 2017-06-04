d4d702f476cc6e35d51960baa02f4e6bb3dded7e
watsonb8 <watsonb8@mail.uc.edu>
2017-06-04 18:28:54 -0400

    Initial hueprovider commit
    Initial hueprovider commit

---
+++ JarvisConsole/JarvisAPI/DataProviders/HueDataProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Q42.HueApi.Interfaces;
+using Q42.HueApi;
+using Q42.HueApi.Models.Bridge;
+
+namespace JarvisAPI.DataProviders
+{
+    public static class HueDataProvider
+    {
+        #region Public Methods
+        public static async void Initialize()
+        {
+            //Locate bridge
+            IBridgeLocator locator = new HttpBridgeLocator();
+            IEnumerable<LocatedBridge> bridgeIPs = await locator.LocateBridgesAsync(TimeSpan.FromSeconds(5));
+
+            //Registe bridge
+            ILocalHueClient client = new LocalHueClient(bridgeIPs.FirstOrDefault().IpAddress);
+            var appKey = await client.RegisterAsync("JarvisAI", "HueHub");
+
+            client.Initialize(appKey);
+        }
+
+        #endregion Public Methods
+    }
+}
